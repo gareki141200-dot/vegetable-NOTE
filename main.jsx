9ebd07b10763a538e60396c39236174f3e117b7b
gareki141200-dot <gareki.141200@gmail.com>
2026-07-28 08:08:54 +0900

    main.jsx を更新

--- main.jsx
+++ main.jsx
@@ -1,1 +1,12 @@
- 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { installStoragePolyfill } from "./storage.js";
+
+installStoragePolyfill();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
