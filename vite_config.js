9d24967f1324126b003a656ea0ec25bd2a725167
gareki141200-dot <gareki.141200@gmail.com>
2026-07-27 22:07:35 +0900

    vite_config.js を更新

--- vite_config.js
+++ vite_config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // GitHub Pagesは https://<user>.github.io/vegetable-NOTE/ のようにサブフォルダで公開されるため、
-  // ここを "/" のままにすると、ビルド後のJS/CSSが見つからず白い画面になる。
+  // GitHub Pagesのサブフォルダ名に合わせる
   base: "/vegetable-NOTE/",
   plugins: [react()],
 });
