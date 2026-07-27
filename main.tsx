ab7751921d9a2682fc17dcd1e2aa47c1503bf4cb
gareki141200-dot <gareki.141200@gmail.com>
2026-07-27 16:29:17 +0900

    main.tsx を更新

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { installStoragePolyfill } from "./storage.js";
+import App from "./App";
+import { installStoragePolyfill } from "./storage";
 
 installStoragePolyfill();
 
